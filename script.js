326c1b2b55071c0ffa5a97f989f0c4687a7916aa
Pratyusha Parimi <pratyushaparimi2@gmail.com>
2025-10-21 10:17:49 +0200

    Added Js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const submitBtn = document.querySelector(".modal-footer .btn-primary");
+  submitBtn.addEventListener("click", function() {
+    alert("Thank you for signing up!");
+  });
+});
